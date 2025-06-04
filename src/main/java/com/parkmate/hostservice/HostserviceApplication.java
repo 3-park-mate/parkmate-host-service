323f63153e8ca361dd869851f7eae0b6d5e7ddd3
bomikim <kbm8527@gmail.com>
2025-06-05 08:58:34 +0900

    초기 설정

---
+++ src/main/java/com/parkmate/hostservice/HostserviceApplication.java
@@ -0,0 +1,13 @@
+package com.parkmate.hostservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parkmate/hostservice/HostserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkmate.hostservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
